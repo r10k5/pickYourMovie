2260c9a6d79de0032b1f6dcfa9d1d33acaa51cb4
5ppwupp5 <dashadodik2285@gmail.com>
2023-10-05 21:13:26 +0300

    верстка главной страницы

--- pickYourMovie/src/views/HomeView.vue
+++ pickYourMovie/src/views/HomeView.vue
@@ -1,16 +1,66 @@
 <script setup lang="ts">
+import AppIcon from '@/components/icons/AppIcon.vue';
 </script>
 
 <template>
-  <main>
-    <div class="square"> </div>
-  </main>
+  <div class="text"> 
+    <h2 class="header">
+      Подберем фильм для вашего вечера
+    </h2>
+    <p class="description">
+      Сервис подберет наилучшее решение для всей семьи бла бла бла
+    </p>
+    <button type="button" class="button">
+      Попробовать
+    </button>
+  </div>
+  <div class="image">
+    <AppIcon width="auto" height="auto" />
+  </div>
 </template>
 
 <style scoped> 
-.square{
-  height: 100px;
-  width: 200px;
-  background-color: rgb(200,110,205);
+.text {
+  display: flex;
+  flex-direction: column;
+}
+.text, .image {
+  width: 100%;
+}
+.header {
+  color: #EBEBEB;
+  font-size: 80px;
+  font-weight: 400;
+  margin: 0;
+  margin-bottom: 44px;
+}
+.description {
+  color: #EBEBEB;
+  font-size: 40px;
+  font-weight: 400;
+  margin: 0;
+}
+.button {
+  border-radius: 60px;
+  background: linear-gradient(180deg, #E8505B 0%, #8B3037 100%);
+  margin: auto 0;
+  color: #FFF;
+  font-size: 40px;
+  font-weight: 400;
+  border: none;
+  width: 100%;
+  padding: 36px 0px;
+}
+@media (max-width: 1780px) {
+  .description {
+    font-size: 20px;
   }
+  .header {
+    font-size: 40px;
+  }
+  .button {
+    font-size: 20px;
+  }
+}
+
 </style>
