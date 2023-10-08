2abcf862f6c959e44d27266e0402e8a2cda8880f
r10k5 <r10k5.job@gmail.com>
2023-10-08 19:04:03 +0300

    Чистка и фикс

--- pickYourMovie/src/views/HomeView.vue
+++ pickYourMovie/src/views/HomeView.vue
@@ -3,7 +3,7 @@ import AppIcon from '@/components/icons/AppIcon.vue';
 </script>
 
 <template>
-  <div class="text"> 
+  <div class="text-content"> 
     <h2 class="header">
       Подберем фильм для вашего вечера
     </h2>
@@ -15,41 +15,50 @@ import AppIcon from '@/components/icons/AppIcon.vue';
     </button>
   </div>
   <div class="image">
-    <AppIcon width="auto" height="auto" />
+    <AppIcon width="100%" height="100%" />
   </div>
 </template>
 
 <style scoped> 
-.text {
+.text-content {
   display: flex;
   flex-direction: column;
+  max-width: 764px;
+  padding: 103px 0 76px 64px;
 }
-.text, .image {
+.text-content, .image {
   width: 100%;
+}
+.image {
+  padding: 68px 64px 12px 0;
 }
 .header {
   color: #EBEBEB;
-  font-size: 80px;
+  font-size: 72px;
   font-weight: 400;
   margin: 0;
   margin-bottom: 44px;
 }
 .description {
-  color: #EBEBEB;
+  color: #DCDCDC;
   font-size: 40px;
   font-weight: 400;
   margin: 0;
+  font-style: normal;
+  line-height: normal;
 }
 .button {
   border-radius: 60px;
   background: linear-gradient(180deg, #E8505B 0%, #8B3037 100%);
-  margin: auto 0;
+  margin: auto 0 0 0;
   color: #FFF;
   font-size: 40px;
   font-weight: 400;
   border: none;
   width: 100%;
   padding: 36px 0px;
+  box-shadow: 0px 4px 4px 0px rgba(33, 23, 23, 0.50);
+
 }
 @media (max-width: 1780px) {
   .description {
